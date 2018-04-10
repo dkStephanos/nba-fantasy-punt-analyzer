1fe452617577c85a26686e896ce7df47f7c06b41
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-04-10 18:27:23 -0400

    Deleted excess code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,30 +3,5 @@ require 'Authenticator'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def yahoo
-  	#byebug
-  	authenticator = Authenticator.new
-    token = authenticator.yahoo(params[:code])
-    # Generate token...
-    #token = TokiToki.encode(login)
-    # ... create user if it doesn't exist...
-    #User.where(login: login).first_or_create!(
-    #  name: name,
-    #)
-    # ... and redirect to client app.
-    redirect_to "#{auth}?token=#{token}"
-  rescue StandardError => error
-    redirect_to "#{auth}?error=#{error.message}"
-  end
-
-  private
-
-  def issuer
-    ENV['NBA_FPA_CLIENT_URL']
-  end
-
-  def auth
-    ENV['NBA_FPA_YAHOO_CALLBACK_URL']
-  end
+  
 end
