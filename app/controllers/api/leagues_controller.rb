87717b80bab11bb72f7daa5237745c7a0c66386f
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-04-14 16:00:57 -0400

    Moved YahooApi initialization into before_action

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -1,18 +1,23 @@
 require 'YahooApi'
 
 class Api::LeaguesController < ApplicationController
+  before_action :initialize_api
+
   def user_leagues
-  	token = request.headers[:Authorization]
-    yahooApi = YahooApi.new(token)
-    leagues = yahooApi.user_leagues
+    leagues = @yahooApi.user_leagues
   	render json: leagues
   end
 
   def players
-    token = request.headers[:Authorization]
     league_key = request.headers[:LeagueKey]
-    yahooApi = YahooApi.new(token)
-    players = yahooApi.players(league_key, 1)
+    players = @yahooApi.players(league_key, 1)
     render json: players
+  end
+
+  private
+
+  def initialize_api
+    token = request.headers[:Authorization]
+    @yahooApi = YahooApi.new(token)
   end
 end
