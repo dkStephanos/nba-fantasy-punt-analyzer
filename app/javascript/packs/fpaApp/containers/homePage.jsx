e00a513896b5b79a9a57841db38f93e4d9636c8a
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-04-14 20:47:43 -0400

    Added Pager for pagination

--- app/javascript/packs/fpaApp/containers/homePage.jsx
+++ app/javascript/packs/fpaApp/containers/homePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Pager } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { getLeagueByKey } from '../actions/league';
 import { getFreeAgents } from '../actions/player';
@@ -10,7 +10,7 @@ class HomePage extends React.Component {
   componentWillMount() {
   	const leagueKey = middleware.getLeagueKey();
 	//this.props.getLeagueByKey(leagueKey);
-	this.props.getFreeAgents(leagueKey, 1);
+	this.props.getFreeAgents(leagueKey, 26);
   }
 
   render() {
@@ -22,7 +22,7 @@ class HomePage extends React.Component {
     return (
       <div>
         <h1>NBA Fantasy Analyzer App</h1>
-  		<Table striped bordered hover>
+  		<Table striped bordered responsive hover>
 		  <thead>
 		    <tr>
 		      <th>Rank</th>
@@ -35,6 +35,10 @@ class HomePage extends React.Component {
 		    {playerRows}
 		  </tbody>
 		</Table>
+		<Pager>
+  			<Pager.Item href="#">Previous</Pager.Item>{' '}
+  			<Pager.Item href="#">Next</Pager.Item>
+		</Pager>
       </div>
     );
   }
