3e820aa079c3b1cd408468b06770b40ed3c42082
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-04-23 19:09:47 -0400

    Changed image href to src

--- app/javascript/packs/fpaApp/components/team.jsx
+++ app/javascript/packs/fpaApp/components/team.jsx
@@ -7,9 +7,9 @@ class Team extends React.Component {
     return(
       <div key={this.props.team.team_key}>
         <h3>{this.props.team.name}</h3>
-        <img href={this.props.team.team_logos.team_logo.url}></img>
+        <img src={this.props.team.team_logos.team_logo.url}></img>
         <p>Manager: {this.props.team.managers.manager.nickname}</p>
-        <img href={this.props.team.managers.manager.image_url}></img>
+        <img src={this.props.team.managers.manager.image_url}></img>
         <p>Scoring Type: {this.props.team.league_scoring_type}</p>
         <a href={this.props.team.url}>To Team</a>
       </div>
