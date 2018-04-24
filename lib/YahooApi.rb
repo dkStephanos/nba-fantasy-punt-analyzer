d553138d5357ebccc5ff1e1c54facc70a4256dfe
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-04-24 10:56:11 -0400

    Refactored/Commented API requests methods... added minimun check for user leagues/teams

--- lib/YahooApi.rb
+++ lib/YahooApi.rb
@@ -3,6 +3,7 @@ class YahooApi
   base_uri ENV['YAHOO_API_BASE_URL']
 
   def initialize(access_token)
+    # Sets headers with user's JWT for future API requests
     @headers = {
       'Authorization' => access_token,
       'Content-Type' => 'application/json'
@@ -10,36 +11,46 @@ class YahooApi
   end
 
   def user_leagues
+    # Get all leagues a user is in
     resp = self.class.get(
       ENV['YAHOO_API_LEAGUES_URL'],
       :headers => @headers)
+    # Convert from XML to JSON
     respJSON = Hash.from_xml(resp.body).as_json
+    # Check to make sure there is more than 1 league
     if(respJSON["fantasy_content"]["users"]["user"]["games"]["count"].to_i > 1)
+      # For each league, if it is in the current year, add it to leagues
       respJSON["fantasy_content"]["users"]["user"]["games"].each do |game|
         if(game.game_key == ENV['GAME_ID'])
           leagues = game["leagues"]["league"].to_json
         end
       end
     else
-      if(respJSON["fantasy_content"]["users"]["user"]["games"]["game"]["game_key"] == ENV['GAME_ID'])
+      # If there is only one league, check if it is current
+      if(respJSON["fantasy_content"]["users"]["user"]["games"]["count"].to_i == 1)
+        if(respJSON["fantasy_content"]["users"]["user"]["games"]["game"]["game_key"] == ENV['GAME_ID'])
           leagues = respJSON["fantasy_content"]["users"]["user"]["games"]["game"]["leagues"]["league"].to_json
         end
+      end
     end
+    # If we have any leagues, return them, else return an error message
     if(!leagues.empty?)
-      byebug
       leagues
     else
-      leagues = "Error fetching league data"
+      leagues = "No current leagues found"
     end
   end
 
   def free_agents(league_key, start)
+    # Makes a request for all Free Agents stats from users league, sorted by overall rank
     resp = self.class.get(
       ENV['YAHOO_API_PLAYERS_URL'] + league_key + "/players/stats",
       :headers => @headers,
       :query => { start: start, sort: 'OR', status: 'FA' })
+    # Convert from XML to JSON
     respJSON = Hash.from_xml(resp.body).as_json
-    if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 1)
+    # Checks to make sure we got player data, returning it if so, otherwise an error message
+    if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 0)
       players = respJSON["fantasy_content"]["league"]["players"]["player"]
     else
       players = "Error fetching players data"
@@ -54,8 +65,10 @@ class YahooApi
         ENV['YAHOO_API_PLAYERS_URL'] + league_key + "/players/stats",
         :headers => @headers,
         :query => { start: n, sort: 'OR', status: 'ALL' })
+      # Convert from XML to JSON
       respJSON = Hash.from_xml(resp.body).as_json
-      if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 1)
+       # Checks to make sure we got player data, returning it if so, otherwise an error message
+      if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 0)
         players += respJSON["fantasy_content"]["league"]["players"]["player"]
       else
         players = "Error fetching players data"
@@ -68,9 +81,10 @@ class YahooApi
     resp = self.class.get(
       ENV['YAHOO_API_TEAM_URL'] + team_key + "/players/stats",
       :headers => @headers)
+    # Convert from XML to JSON
     respJSON = Hash.from_xml(resp.body).as_json
-    byebug
-    if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 1)
+     # Checks to make sure we got player data, returning it if so, otherwise an error message
+    if(respJSON["fantasy_content"]["league"]["players"]["count"].to_i > 0)
       team = respJSON["fantasy_content"]["league"]["players"]["player"]
     else
       team = "Error fetching team data"
@@ -82,18 +96,24 @@ class YahooApi
     resp = self.class.get(
       ENV['YAHOO_API_TEAMS_URL'],
       :headers => @headers)
+    # Convert from XML to JSON
     respJSON = Hash.from_xml(resp.body).as_json
+    # Checks if there are multiple teams, only adding those that are current
     if(respJSON["fantasy_content"]["users"]["user"]["teams"]["count"].to_i > 1)
       respJSON["fantasy_content"]["users"]["user"]["teams"]["team"].each do |team|
-        if(team["team_key"].split('.').first.to_i >= ENV['GAME_ID'].to_i)
+        if(team["team_key"].split('.').first == ENV['GAME_ID'])
           teams << team
         end
       end
     else
-      if(respJSON["fantasy_content"]["users"]["user"]["teams"]["team"]["team_key"].split('.').first.to_i >= ENV['GAME_ID'].to_i)
+    # If only one team, check if it is current and if it is, return it
+    if(respJSON["fantasy_content"]["users"]["user"]["teams"]["count"].to_i == 1)
+      if(respJSON["fantasy_content"]["users"]["user"]["teams"]["team"]["team_key"].split('.').first == ENV['GAME_ID'])
           teams = respJSON["fantasy_content"]["users"]["user"]["teams"]["team"]
         end
+      end
     end
+    # If we have teams, return them, else return an error message
     if(!teams.empty?)
       teams
     else
