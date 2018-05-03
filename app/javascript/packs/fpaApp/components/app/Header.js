28c24ab9263f22652b37fdc10704bd455aafcf28
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-03 17:18:46 -0400

    Changed header to a responsive  bootstrap NavBar

--- app/javascript/packs/fpaApp/components/app/Header.js
+++ app/javascript/packs/fpaApp/components/app/Header.js
@@ -1,14 +1,37 @@
 import React, {PropTypes} from 'react';  
-import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const Header = () => {  
   return (
-    <nav>
-      <Link to="/" 
-        activeClassName="active">Home</Link>
-      {" | "}
-      <Link to="/stats" activeClassName="active">Stats</Link>
-    </nav>
+    <Navbar inverse collapseOnSelect>
+	  <Navbar.Header>
+	    <Navbar.Brand>
+	      <a href="#home">Fantasy Punt Analyzer</a>
+	    </Navbar.Brand>
+	    <Navbar.Toggle />
+	  </Navbar.Header>
+	  <Navbar.Collapse>
+	    <Nav>
+	      <NavItem eventKey={1} href="#">
+	        My Team
+	      </NavItem>
+
+	      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+	        <MenuItem eventKey={3.1}>Action</MenuItem>
+	        <MenuItem eventKey={3.2}>Another action</MenuItem>
+	        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+	        <MenuItem divider />
+	        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+	      </NavDropdown>
+
+	    </Nav>
+	    <Nav pullRight>
+	      <NavItem eventKey={1} href="#logout">
+	        Logout
+	      </NavItem>
+	    </Nav>
+	  </Navbar.Collapse>
+	</Navbar>
   );
 };
 
