285e763426e1c895afeeb785cb9f4942b296135b
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-04 21:21:09 -0400

    Converted if/else staments to a switch statement using internal state to step through the proccess of fetching/proccessing player data

--- app/javascript/packs/fpaApp/containers/playerFetchTransition.jsx
+++ app/javascript/packs/fpaApp/containers/playerFetchTransition.jsx
@@ -10,33 +10,44 @@ class PlayerFetchTransition extends Component {
   	// Initializes boolean to determine when it is safe to calculate z-Scores
     // Fetch League Players
     this.props.getPlayers(middleware.getLeagueKey());
+    this.setState({ step: 1 })
   }
 
   // Used for a series of data fetches. Each if statement checks if that action has already occured
   // so we don't repeat actions. As a result, one if statement worth of actions occur per lifecycle event
   // sequentially until all fetches are done and we are redirected to the home page
   componentDidUpdate() {
-  	// First, we need the FG and FT impacts for each player
-  	if(this.props.players[0].player_stats.stats.stat[this.props.players[0].player_stats.stats.stat.length - 1].stat_id !== "1008") {
-  		this.props.calculateFgAndFtImpacts(this.props.players, this.props.means);
-  	} else if(Object.keys(this.props.means).length === 0) {
-      // After we have the FG/FT impacts, we can determine means for each player's statistical categories 
-      debugger;
-      this.props.calculateMeans(this.props.players);
-    } else if(Object.keys(this.props.stdDeviations).length === 0) {
-  		// Once we have that, we can calculate the standard deviation for each statistical category 
-  		this.props.calculateStdDeviations(this.props.players, this.props.means);
-  	} else if(!Object.keys(this.props.players[0].player_stats.stats.stat[5]).includes("zScore")) {
-  		// Then, once we have the standard deviations, we can calculate the players z-Scores
-  		this.props.calculateZScores(this.props.players, this.props.means, this.props.stdDeviations);
-  	} else if(!Object.keys(this.props.players[0]).includes("rank")) {
-  		// Last step, once we have the z-Scores for the players, we can calculate their rank
-  		this.props.calculatePlayerRanks(this.props.players);
-  	} else {
-      debugger;
-  		// Finally, if we got this far, we have all the data how we need it, so redirect to the home page
-  		this.redirectToHomePage();
-  	}
+    switch(this.state.step) {
+      case 1:
+        // First, we need the FG and FT impacts for each player
+        this.props.calculateFgAndFtImpacts(this.props.players, this.props.means);
+        this.setState({ step: 2 })
+        break;
+      case 2:
+        // After we have the FG/FT impacts, we can determine means for each player's statistical categories 
+        this.props.calculateMeans(this.props.players);
+        this.setState({ step: 3 })
+        break;
+      case 3:
+        // Once we have that, we can calculate the standard deviation for each statistical category 
+        this.props.calculateStdDeviations(this.props.players, this.props.means);
+        this.setState({ step: 4 })
+        break;
+      case 4:
+        // Then, once we have the standard deviations, we can calculate the players z-Scores
+        this.props.calculateZScores(this.props.players, this.props.means, this.props.stdDeviations);
+        this.setState({ step: 5 })
+        break;
+      case 5:
+        // Last step, once we have the z-Scores for the players, we can calculate their rank
+        this.props.calculatePlayerRanks(this.props.players);
+        this.setState({ step: 6 })
+        break;
+      case 6:
+        // Finally, if we got this far, we have all the data how we need it, so redirect to the home page
+        this.redirectToHomePage();
+      default:
+    }
   }
 
   redirectToHomePage = () => {
