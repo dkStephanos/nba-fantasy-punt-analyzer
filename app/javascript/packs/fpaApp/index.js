643ba5cbe07dc3bdfc9358124a6107aa10b4e81d
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 19:39:36 -0400

    Importing bootstrap css

--- app/javascript/packs/fpaApp/index.js
+++ app/javascript/packs/fpaApp/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Routes />, document.getElementById('fpa-app'),
-  )
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Routes />, document.getElementById("fpa-app"));
 });
