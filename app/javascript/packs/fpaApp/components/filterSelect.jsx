023492f80346ac6bfeffad94619e641a269768e2
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 18:56:16 -0400

    Formatted filters

--- app/javascript/packs/fpaApp/components/filterSelect.jsx
+++ app/javascript/packs/fpaApp/components/filterSelect.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { ToggleButtonGroup, ToggleButton, Button, Panel } from 'react-bootstrap';
-
+import React from "react";
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Button,
+  Panel
+} from "react-bootstrap";
 
 class FilterSelect extends React.Component {
-
   constructor(props, context) {
     super(props, context);
 
@@ -27,76 +30,97 @@ class FilterSelect extends React.Component {
 
   render() {
     // We aren't actually using these categories for stat calculation, so we don't want them included
-    const labelBlackList = ['Rank', 'Name', 'Team', 'Positions', 'Owner', 'Status','FG%', 'FT%', 'FGM/FGA', 'FTM/FTA'];
+    const labelBlackList = [
+      "Rank",
+      "Name",
+      "Team",
+      "Positions",
+      "Owner",
+      "Status",
+      "FG%",
+      "FT%",
+      "FGM/FGA",
+      "FTM/FTA"
+    ];
 
-    const categoryCheckboxes = this.props.categoryLabels.filter(function(label){
-      return (labelBlackList.indexOf(label) == -1 ? true : false);
-    }).map(label => (
-      <ToggleButton key={`${label}-checkbox`} value={label}>{label}</ToggleButton>
-    ));
+    const categoryCheckboxes = this.props.categoryLabels
+      .filter(function(label) {
+        return labelBlackList.indexOf(label) == -1 ? true : false;
+      })
+      .map(label => (
+        <ToggleButton key={`${label}-checkbox`} value={label}>
+          {label}
+        </ToggleButton>
+      ));
 
     // Positions in basketball don't change, so we need a checkbox for each of these positions
-    const positionCheckboxes = ['PG', 'SG', 'SF', 'PF', 'C'].map(position => (
-      <ToggleButton key={`${position}-checkbox`} value={position}>{position}</ToggleButton>
+    const positionCheckboxes = ["PG", "SG", "SF", "PF", "C"].map(position => (
+      <ToggleButton key={`${position}-checkbox`} value={position}>
+        {position}
+      </ToggleButton>
     ));
 
     // Create a checkbox for all status conditions
-    const statusCheckboxes = ['O', 'INJ', 'GTD'].map(status => (
-      <ToggleButton key={`${status}-checkbox`} value={status}>{status}</ToggleButton>
+    const statusCheckboxes = ["O", "INJ", "GTD"].map(status => (
+      <ToggleButton key={`${status}-checkbox`} value={status}>
+        {status}
+      </ToggleButton>
     ));
 
-    return(
+    return (
       <div key="filter-select">
-        <Panel id="filter-checkboxes" >
+        <Panel id="filter-checkboxes">
           <Panel.Heading>
-            <Panel.Title toggle>
-              Player Filters
-            </Panel.Title>
+            <Panel.Title toggle>Player Filters</Panel.Title>
           </Panel.Heading>
           <Panel.Collapse>
             <Panel.Body>
-              <div id="stat-filter-checkboxes" >
-          <label>Categories: </label>
-          <ToggleButtonGroup
-            type="checkbox"
-            
-            onChange={this.handleCategoryChange}
-          >
-            {categoryCheckboxes}
-          </ToggleButtonGroup>
-        </div>
+              <div id="checkbox-container">
+                <div style={{ padding: "1%" }} id="stat-filter-checkboxes">
+                  <label>Categories: </label>
+                  <br />
+                  <ToggleButtonGroup
+                    type="checkbox"
+                    onChange={this.handleCategoryChange}
+                  >
+                    {categoryCheckboxes}
+                  </ToggleButtonGroup>
+                </div>
 
-        <div id="position-filter-checkboxes">
-          <label>Positions: </label>
-          <ToggleButtonGroup
-            type="checkbox"
-            
-            onChange={this.handlePositionChange}
-          >
-            {positionCheckboxes}
-          </ToggleButtonGroup>
-        </div>
+                <div style={{ padding: "1%" }} id="position-filter-checkboxes">
+                  <label>Positions: </label>
+                  <br />
+                  <ToggleButtonGroup
+                    type="checkbox"
+                    onChange={this.handlePositionChange}
+                  >
+                    {positionCheckboxes}
+                  </ToggleButtonGroup>
+                </div>
 
-        <div id="status-filter-checkboxes">
-          <label>Statuses: </label>
-          <ToggleButtonGroup
-            type="checkbox"
-            
-            onChange={this.handleStatusChange}
-          >
-            {statusCheckboxes}
-          </ToggleButtonGroup>
-        </div>
-        <Button bsStyle="primary" onClick={() => this.handleClick(this.state)}>
-          Apply Filters
-        </Button>
+                <div style={{ padding: "1%" }} id="status-filter-checkboxes">
+                  <label>Statuses: </label>
+                  <br />
+                  <ToggleButtonGroup
+                    type="checkbox"
+                    onChange={this.handleStatusChange}
+                  >
+                    {statusCheckboxes}
+                  </ToggleButtonGroup>
+                </div>
+              </div>
+              <Button
+                bsStyle="primary"
+                style={{ float: "right", display: "inline-block" }}
+                onClick={() => this.handleClick(this.state)}
+              >
+                Apply Filters
+              </Button>
             </Panel.Body>
           </Panel.Collapse>
         </Panel>
-
-        
       </div>
-    )
+    );
   }
 }
 export default FilterSelect;
