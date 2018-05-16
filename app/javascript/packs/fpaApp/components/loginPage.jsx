8c81f6dd2b2ce2dd22b78ef7f2313bcd02948e62
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 17:56:17 -0400

    Added short description and placed contents in Jumbotron

--- app/javascript/packs/fpaApp/components/loginPage.jsx
+++ app/javascript/packs/fpaApp/components/loginPage.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button, Jumbotron } from "react-bootstrap";
 
-const authorizeUrl = 'https://api.login.yahoo.com/oauth2/request_auth';
+const authorizeUrl = "https://api.login.yahoo.com/oauth2/request_auth";
 const clientId = process.env.CLIENT_ID;
-const redirectUri = 'http://stephanos.pagekite.me/auth'
-const responseType = 'token'
-const language = 'en-us';
+const redirectUri = "http://stephanos.pagekite.me/auth";
+const responseType = "token";
+const language = "en-us";
 
 class LoginPage extends React.Component {
-
   render() {
-    return(
-      <div>
-        <h1>NBA Fantasy Analyzer</h1>
-  		<Button 
-  			href={`${authorizeUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&language=${language}`}
-  			bsStyle="primary">
-  			Log In
-  		</Button>
-      </div>
-    )
+    return (
+      <Jumbotron>
+        <h1>Welcome to the Fantasy Punt Analyzer!</h1>
+        <p>
+          Being hard at everything is hard, especially in fantasy basketball.
+          That is why we here at FPA believe in the holy act of punting
+          statistical categories! We use live data from your Yahoo Sports league
+          to dynamically rank and sort players with a custom algorithm that
+          allows users to filter out categories. Just log in, select your team,
+          and you're good to go!
+        </p>
+        <span>
+          <Button
+            href={`${authorizeUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&language=${language}`}
+            bsStyle="primary"
+          >
+            Log In
+          </Button>
+        </span>
+      </Jumbotron>
+    );
   }
 }
 export default LoginPage;
