b289ecd1004310e0e1c1eec4783411fdd4ce6fe3
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 18:14:38 -0400

    Moved body into Jumbotron

--- app/javascript/packs/fpaApp/components/app/notFound.jsx
+++ app/javascript/packs/fpaApp/components/app/notFound.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-
+import React from "react";
+import { Button, Jumbotron } from "react-bootstrap";
 
 class NotFound extends React.Component {
-
   render() {
-    return(
-      <div>
+    return (
+      <Jumbotron>
         <h1>Air Ball...</h1>
         <p>This page doesn't exist</p>
-  		<Button 
-  			href={`/home`}
-  			bsStyle="primary">
-  			Back Home
-  		</Button>
-      </div>
-    )
+        <Button href={`/home`} bsStyle="primary">
+          Back Home
+        </Button>
+      </Jumbotron>
+    );
   }
 }
 export default NotFound;
