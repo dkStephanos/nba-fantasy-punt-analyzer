4dcfc3c55d3d7f75d36e542df62e7ab325076dcf
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 15:34:49 -0400

    Created icon version of logo, added to favicon and app Header

--- app/javascript/packs/fpaApp/components/app/Header.js
+++ app/javascript/packs/fpaApp/components/app/Header.js
@@ -1,38 +1,45 @@
-import React, {PropTypes} from 'react';  
-import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import React, { PropTypes } from "react";
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import fpaLogo from "../../assets/images/icons/CircleLogo.ico";
 
-const Header = () => {  
+const Header = () => {
   return (
     <Navbar inverse collapseOnSelect>
-	  <Navbar.Header>
-	    <Navbar.Brand >
-	        <a href="/home" >Fantasy Punt Analyzer</a>
-	    </Navbar.Brand>
-	    <Navbar.Toggle />
-	  </Navbar.Header>
-	  <Navbar.Collapse>
-	    <Nav>
-	      <NavItem eventKey={1} href="/myTeam">
-	        My Team
-	      </NavItem>
+      <Navbar.Header>
+        <img
+          src={fpaLogo}
+          className="pull-left"
+          style={{ maxWidth: "60px", padding: "3%", paddingRight: "5%" }}
+        />
+        <Navbar.Brand>
+          <a href="/home">Fantasy Punt Analyzer</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="/myTeam">
+            My Team
+          </NavItem>
 
-	      <NavDropdown eventKey={3} title="Actions" id="basic-nav-dropdown">
-	        <MenuItem href="/teamSelect" eventKey={3.1}>Team Select</MenuItem>
-	        <MenuItem eventKey={3.2}>Another action</MenuItem>
-	        <MenuItem eventKey={3.3}>Settings</MenuItem>
-	        <MenuItem divider />
-	        <MenuItem eventKey={3.3}>About</MenuItem>
-	      </NavDropdown>
-
-	    </Nav>
-	    <Nav pullRight>
-	      <NavItem eventKey={1} href="/logout">
-	        Logout
-	      </NavItem>
-	    </Nav>
-	  </Navbar.Collapse>
-	</Navbar>
+          <NavDropdown eventKey={3} title="Actions" id="basic-nav-dropdown">
+            <MenuItem href="/teamSelect" eventKey={3.1}>
+              Team Select
+            </MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Settings</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>About</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="/logout">
+            Logout
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Header;  
+export default Header;
