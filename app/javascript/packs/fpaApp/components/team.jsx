9aabc0b42259a42258f61be53955ef1a9ba7cdf8
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-16 20:03:22 -0400

    Further styling, but a divider between button and contents of card

--- app/javascript/packs/fpaApp/components/team.jsx
+++ app/javascript/packs/fpaApp/components/team.jsx
@@ -5,18 +5,25 @@ class Team extends React.Component {
   render() {
     return (
       <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <img src={this.props.team.team_logos.team_logo.url} alt="Team Logo" />
-          <div className="caption" style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center" }} className="thumbnail">
+          <img
+            style={{ paddingTop: "3%" }}
+            src={this.props.team.team_logos.team_logo.url}
+            alt="Team Logo"
+          />
+          <div
+            className="caption"
+            style={{ borderBottom: "1px solid #D4D4D4" }}
+          >
             <h3>{this.props.team.name}</h3>
             <p>Manager: {this.props.team.managers.manager.nickname}</p>
             <p>Scoring Type: {this.props.team.league_scoring_type}</p>
-            <p>
-              <Button bsStyle="primary" href={this.props.team.url}>
-                Go to Yahoo!
-              </Button>
-            </p>
           </div>
+          <p style={{ paddingTop: "3%" }}>
+            <Button bsStyle="primary" href={this.props.team.url}>
+              Go to Yahoo!
+            </Button>
+          </p>
         </div>
       </div>
     );
