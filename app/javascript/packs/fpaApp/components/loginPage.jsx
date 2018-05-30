ffa65b6dc128e381290600796de53972d0dd7936
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-30 17:53:21 -0400

    Added logo and style to login page

--- app/javascript/packs/fpaApp/components/loginPage.jsx
+++ app/javascript/packs/fpaApp/components/loginPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Jumbotron } from "react-bootstrap";
+import logo from "../assets/images/icons/Circle256.gif";
 
 const authorizeUrl = "https://api.login.yahoo.com/oauth2/request_auth";
 const clientId = process.env.CLIENT_ID;
@@ -11,23 +12,25 @@ class LoginPage extends React.Component {
   render() {
     return (
       <Jumbotron>
-        <h1>Welcome to Fantasy Punt Analyzer!</h1>
-        <p>
-          Being good at everything is hard, especially in fantasy basketball.
-          That is why we here at FPA believe in the holy act of punting
-          statistical categories! We use live data from your Yahoo Sports league
-          to dynamically rank and sort players with a custom algorithm that
-          allows users to filter out categories. Just log in, select your team,
-          and you're good to go!
-        </p>
-        <span>
+        <div style={{ textAlign: "center" }}>
+          <img src={logo} />
+          <h1>Welcome to Fantasy Punt Analyzer!</h1>
+          <p>
+            Being good at everything is hard, especially in fantasy basketball.
+            That is why we here at FPA believe in the holy act of punting
+            statistical categories! We use live data from your Yahoo Sports
+            league to dynamically rank and sort players with a custom algorithm
+            that allows users to filter out categories. Just log in, select your
+            team, and you're good to go!
+          </p>
           <Button
             href={`${authorizeUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&language=${language}`}
             bsStyle="primary"
+            bsSize="large"
           >
             Log In
           </Button>
-        </span>
+        </div>
       </Jumbotron>
     );
   }
