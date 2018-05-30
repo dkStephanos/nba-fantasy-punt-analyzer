1cf44944c6b8100a299edcf489ce91daa3dabd10
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-30 14:40:24 -0400

    Moved current filters to statReducer

--- app/javascript/packs/fpaApp/reducers/statReducer.js
+++ app/javascript/packs/fpaApp/reducers/statReducer.js
@@ -4,22 +4,22 @@ const initialState = {
   categoryLabels: [],
   filteredMeans: {},
   filteredStdDeviations: {},
-  filteredCategoryLabels: []
+  currentFilters: { categories: [], positions: [], statuses: [] }
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_MEANS':
+    case "SET_MEANS":
       return Object.assign({}, state, {
         means: action.means
       });
 
-    case 'SET_STD_DEVIATIONS':
+    case "SET_STD_DEVIATIONS":
       return Object.assign({}, state, {
         stdDeviations: action.stdDeviations
       });
 
-    case 'SET_CATEGORY_LABELS':
+    case "SET_CATEGORY_LABELS":
       return Object.assign({}, state, {
         categoryLabels: action.categoryLabels
       });
